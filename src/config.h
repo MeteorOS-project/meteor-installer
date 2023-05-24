708532a2c597115f2d4c4ce04e0aed1919e56bdb
iiab-school <117210149+iiab-school@users.noreply.github.com>
2023-05-24 12:36:37 -0700

    Created main files

---
+++ src/config.h
@@ -0,0 +1,13 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+// INSTALLER CONFIGURATIONS
+#define INSTALLER_NAME             LukeI
+#define INSTALLER_VERSION          1.0
+#define INSTALLER_VERTITLE         Tiger
+
+// FILE CONFIGS
+#define MAINCLASS                  main.c
+
+
+#endif
